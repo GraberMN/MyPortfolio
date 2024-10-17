dede0bd321a2dd2c34ba610216604f91fb2a7861
Mateo Graber <grabermn3@gmail.com>
2024-10-17 13:05:30 -0400

    commit on "main"

--- portfolioScript.js
+++ portfolioScript.js
@@ -1,5 +1,5 @@
 function alertOnReturn() {
-    if (document.referrer != "") {
+    if (document.referrer == "https://formsubmit.co/form/submission") {
         alert("Your message has been sent. You will receive a response from grabermn3@gmail.com in 1-2 business days. Thank you for your time.");
     }
 }
