b08ba54b298b98882f37b9dbb2e0ec520336a570
Mateo Graber <grabermn3@gmail.com>
2024-10-21 11:57:05 -0400

    commit

--- portfolioScript.js
+++ portfolioScript.js
@@ -146,11 +146,11 @@ function underlineCurrentSection() {
             underlineSection("ActualProjectsButton");
             k = 2;
         }
-        else if (scrollTop > 1540 && scrollTop <= 1945) {
+        else if (scrollTop > 1540 && scrollTop <= 1845) {
             underlineSection("ActualSkillsButton");
             k = 3;
         }
-        else if (scrollTop > 1945) {
+        else if (scrollTop > 1845) {
             underlineSection("ActualContactMeButton");
             k = 4;
         }
